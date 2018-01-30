edea765106ed8ec4f1ba7d4f7dc33aa7377acc1e
Zev Isert <dev@zevisert.ca>
2018-01-30 15:43:54 -0800

    LCD Class added and tty moved from Makefile to env file LCD class is just an example, much of this new class's functionality is just wrapped calls

--- project1/project1.ino
+++ project1/project1.ino
@@ -1,10 +1,29 @@
-void setup() {
-    pinMode(13, OUTPUT);
+#include "LCDKeypad.h"
+#include <LiquidCrystal.h>
+
+LCDKeypad pad;
+String strings[LCDKeypad::LCD_BUTTONS::COUNT_BUTTONS] = {
+    "--      ",
+    "RIGHT   ",
+    "UP      ",
+    "DOWN    ",
+    "LEFT    ",
+    "SELECT  ",
+};
+
+void setup()
+{
+    pad = LCDKeypad();
+    pad.clear();
 }
 
-void loop() {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+void loop()
+{
+    pad.print(LCDKeypad::LCD_ROW::TOP, "Hello!");
+
+    // Make sure getLastButton is called first, pollButtons will update it
+    if(pad.getLastButton() != pad.pollButtons()); {
+        // update the display
+        pad.print(LCDKeypad::LCD_ROW::BOTTOM, strings[pad.getLastButton()]);
+    }
 }
