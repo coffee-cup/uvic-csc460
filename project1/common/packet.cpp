e279a654177b246d88357f4b59e33828ea7d45e2
Zev Isert <zev.isert@gmail.com>
2018-02-05 15:52:20 -0800

    Meant to be uint8_t on the laser

--- project1/common/packet.cpp
+++ project1/common/packet.cpp
@@ -2,7 +2,7 @@
 
 #include <stdint.h>
 
-Packet create_packet(uint16_t speedX, uint16_t speedY, uint16_t laserOn) {
+Packet create_packet(uint16_t speedX, uint16_t speedY, uint8_t laserOn) {
     Packet p = {
         .speedX = speedX,
         .speedY = speedY,
