f62cada6e779481cb2f836dbb9a73bd16b03772b
Jake Runzer <jakerunzer@gmail.com>
2018-03-14 19:29:09 -0700

    macro for creating tests

--- project2/libs/tests/test_utils.h
+++ project2/libs/tests/test_utils.h
@@ -1,9 +1,14 @@
 #ifndef _TEST_UTILS_H_
 #define _TEST_UTILS_H_
 
-#define Assert(expr)                            \
-    {                                           \
-        if (!(expr)) while (TRUE);              \
+#include "uart.h"
+
+#define Assert(expr)                         \
+    {                                        \
+        if (!(expr)) {                       \
+            UART_print("Assert Failed!\n");  \
+            for (;;) {}                      \
+        }                                    \
     }
 
 #define AssertNeverCalled()                     \
